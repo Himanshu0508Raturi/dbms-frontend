d7ebfcb13ded1a846241ab776294617c216ac3a9
Himanshu_Raturi <raturihimanshu077@gmail.com>
2025-11-11 23:54:34 +0530

    added render deployment backend link

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -6,7 +6,7 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import { toast } from 'sonner';
 import { CreditCard, Loader2 } from 'lucide-react';
 
-const BASE_URL = 'http://ec2-98-84-15-104.compute-1.amazonaws.com:8080';
+const BASE_URL = 'https://ilibrary-backend.onrender.com';
 
 const Payment = () => {
   const { token, isAuthenticated } = useAuth();
